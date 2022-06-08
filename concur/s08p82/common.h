9cb78a3fe841d560b149bb07e4cb1c531b6827d2
Baris Erkus <bariserkus@gmail.com>
2022-06-08 18:01:20 +0300

    Thread Pool

---
+++ concur/s08p82/common.h
@@ -0,0 +1,8 @@
+//
+// Created by bariserkus on 08.06.2022.
+//
+
+#ifndef S08P82_COMMON_H
+#define S08P82_COMMON_H
+
+#endif //S08P82_COMMON_H
---
+++ concur/s08p82/thread_safe_q.h
@@ -0,0 +1,8 @@
+//
+// Created by bariserkus on 08.06.2022.
+//
+
+#ifndef S08P82_THREAD_SAFE_Q_H
+#define S08P82_THREAD_SAFE_Q_H
+
+#endif //S08P82_THREAD_SAFE_Q_H
